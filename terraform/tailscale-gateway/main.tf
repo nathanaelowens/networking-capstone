f6016780a973bfca6fa07a8993a891d3011e3f07
nathan <accounts@owens.life>
2025-10-27 09:21:54 -0500

    Add tailscale gateway for proxmox

---
+++ terraform/tailscale-gateway/main.tf
@@ -0,0 +1,24 @@
+# main.tf
+module "prox-vm" {
+    source = "../modules/prox-vm"
+
+    vm_id = 124
+    hostname = "ipamserver"
+    domain   = "lanbeforetime.link"
+    ip_address = "192.168.10.24/24"
+    ip_default_gateway = "192.168.10.1"
+    search_domains = ["lanbeforetime.link"]
+    dns_servers = ["192.168.10.20","192.168.10.21"]
+    description = "Tailscale Subnet Router"
+    tags = ["terraform"]
+    auto_poweron = true
+    cloud_config = "debian-trixie_0.1"
+    vm_template = "debian-trixie-proxtanic3-2025-10-13-09-09-38-am"
+    autodetect_net_if = true
+    disk_size = 30 #GB
+    dedicated_memory = 2048 #MB
+    floating_memory = 2048 #MB
+
+    proxmox_node = "proxtanic3"
+    proxmox_fqdn = "proxtanic3.lanbeforetime.link"
+}
