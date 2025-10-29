71c1bf8023fee91fb8881c6af07925d1aeaf1ca8
nathan <accounts@owens.life>
2025-10-29 08:25:48 -0500

    Change logserver to snmpserver

--- terraform/snmpserver/main.tf
+++ terraform/snmpserver/main.tf
@@ -3,13 +3,13 @@ module "prox-vm" {
     source = "../modules/prox-vm"
 
     vm_id = 130
-    hostname = "logserver"
+    hostname = "snmpserver"
     domain   = "lanbeforetime.link"
     ip_address = "192.168.10.30/24"
     ip_default_gateway = "192.168.10.1"
     search_domains = ["lanbeforetime.link"]
     dns_servers = ["192.168.10.20","192.168.10.21"]
-    description = "Syslog Server"
+    description = "SNMP Server"
     tags = ["terraform","backup"]
     auto_poweron = true
     cloud_config = "debian-trixie_0.1"
